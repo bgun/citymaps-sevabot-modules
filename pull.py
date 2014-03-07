f61ab309e6b4a272caa81cfadac7c5488d6f553a
Ben Gundersen <ben@citymaps.com>
2014-03-06 21:39:09 -0500

    pull

--- pull.py
+++ pull.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+# pulls the latest from citymaps-sevabot-modules.
+# Modules are executed from the sevabot directory, hence "cd modules".
+# I'm lazy so just using a nested git folder, not a real submodule.
 import os
 resp = os.system("cd modules;git pull origin master")
 
