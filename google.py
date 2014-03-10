cd3400230eaa320eac9d90d8067320fc52af20a8
Adam Eskreis <adam@citymaps.com>
2014-03-10 18:16:41 -0400

    added burn, google, and wiki

---
+++ google.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+A simple sample module with Skype smileys
+"""
+import os
+import sys
+import urllib
+
+query = urllib.urlencode(" ".join(sys.argv[1:]))
+
+print "https://www.google.com/search?q=" + query
