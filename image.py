8700cc0472f8bc1b233fe5f4f9a75eb19797bb33
Ben Gundersen <ben@citymaps.com>
2014-04-23 11:19:27 -0400

    Image search

---
+++ image.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Performs a Google Image search and returns an image URL.
+Parameters:
+  query - use quotes for multiple words
+  index - from 1 to 8, 8 being the max size of a single page of results
+"""
+
+import sys
+import string
+import random
+import requests
+import json
+
+# help text
+helptext = "Returns the first response from a Google Image search."
+if len(sys.argv) == 1:
+  sys.exit(helptext)
+if len(sys.argv) == 2 and sys.argv[1] == "help":
+  sys.exit(helptext)
+
+
+query = sys.argv[1]
+index = 0
+if len(sys.argv) > 2:
+  try:
+    index = int(sys.argv[2])
+  except ValueError:
+    print "Second parameter should be a number from 1 to 8. Use quotes for multi-word searches."
+    index = 0
+
+def getFirstGoogleImageUrl(q):
+  url = "http://ajax.googleapis.com/ajax/services/search/images"
+  payload = {
+    'v'   : '1.0',
+    'q'   : q,
+    'rsz' : 8
+  }
+  r = requests.get(url, params=payload)
+  j = json.loads(r.text)
+  return j['responseData']['results'][index]['url']
+
+
+if __name__ == '__main__':
+  print getFirstGoogleImageUrl(query)
