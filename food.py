96352246175b42cb0b1d3817a869fd5d1aed0ef0
John Tan <john@citymaps.com>
2014-06-16 19:59:21 -0400

    Fixing url

--- food.py
+++ food.py
@@ -14,9 +14,9 @@ def main():
 
     url = "/search?find_desc="+sys.argv[0]+"&find_loc=10065&ns=1#sortby=rating"
     
-	conn.request("GET", url, None, {
-        "Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-        "User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.117 Safari/537.36"
+    conn.request("GET", "/", None, {
+      "Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+      "User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.117 Safari/537.36"
     })
 
     response = conn.getresponse()
