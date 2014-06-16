beca75a1953fcc47f0f12568b67384d114572a9e
John Tan <john@citymaps.com>
2014-06-16 19:42:09 -0400

    New yelp food search

---
+++ food.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Shows lunch choices from yelp.
+"""
+
+import sys
+import httplib
+from pyquery import PyQuery as pq
+
+def main(args):
+    conn = httplib.HTTPConnection("http://www.yelp.com")
+
+    conn.request("GET", "/search?find_desc="+args[0]+"&find_loc=10065&ns=1#sortby=rating")
+    response = conn.getresponse()
+    html = pq(response.read())
+
+    print html.find(".search-result-title").text()
+
+
+if __name__ == '__main__':
+    main()
